6a4d749e88cdc747671468c4e3701a89735d27b2
Jose Carlos <zecarlos558@gmail.com>
2024-11-01 16:14:28 -0300

    Feat: add funcao remove acentos

--- carteira_tech/app/Helpers/functions.php
+++ carteira_tech/app/Helpers/functions.php
@@ -125,6 +125,10 @@
 
     }
 
+    function removerAcentos($string){
+        return preg_replace(array("/(á|à|ã|â|ä)/","/(Á|À|Ã|Â|Ä)/","/(é|è|ê|ë)/","/(É|È|Ê|Ë)/","/(í|ì|î|ï)/","/(Í|Ì|Î|Ï)/","/(ó|ò|õ|ô|ö)/","/(Ó|Ò|Õ|Ô|Ö)/","/(ú|ù|û|ü)/","/(Ú|Ù|Û|Ü)/","/(ñ)/","/(Ñ)/"),explode(" ","a A e E i I o O u U n N"),$string);
+    }
+
      function formataPesquisa($request)
     {
         if ($request) {
