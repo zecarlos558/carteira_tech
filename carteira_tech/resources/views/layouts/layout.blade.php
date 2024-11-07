2b86d75a345a4f40bef39c4b62bc414336c9967d
Jose Carlos <zecarlos558@gmail.com>
2024-11-06 22:04:30 -0300

    Fix: ajuste import scripts layout

--- carteira_tech/resources/views/layouts/layout.blade.php
+++ carteira_tech/resources/views/layouts/layout.blade.php
@@ -8,7 +8,7 @@
     <title>Carteira Tech | @yield('title')</title>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous" defer></script>
-    <script src="/js/scripts.js" defer></script>
+    <script src="{{ asset('js/scripts.js') }}" defer></script>
 
     <link rel="stylesheet" href="/css/styles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
